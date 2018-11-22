9aa72be7ba9766dbda77c14004c462a39ae626a7
Angelo <angelo.mayer@gmx.de>
2018-11-22 15:38:18 +0100

    mensa wurde hinzugefügt (issue add mensa)

---
+++ places/mensa.js
@@ -0,0 +1,9 @@
+add_polygon("mensa", 'blue', [
+    // Hier die Koordinaten eintragen:
+    // Das Polygon kann aus beliebig vielen Punkten zusammengesetzt werden.
+    // Es sollte aber aus mindestens drei Punkten bestehen.
+    [49.41511820654185, 8.670234205565066],
+    [49.41601115189994, 8.670341493925662],
+    [49.41605998438072, 8.67116761430225],
+	[49.41509030173765, 8.671103241285893],
+]);
